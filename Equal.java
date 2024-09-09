d36f756e99ad4f911de587bc9b611845f260996b
Saptarshi Mondal <saptarshimondal1002@gmail.com>
2024-09-09 17:58:49 +0530

    adding equal

--- Equal.java
+++ Equal.java
@@ -4,8 +4,11 @@ int num1=Integer.parseInt(args[0]);
 int num2=Integer.parseInt(args[1]);
 int num3=Integer.parseInt(args[2]);
 System.out.println("Your enterd number is :"+num1+" "+num2+" "+num3);
-int sum=num1+num2;
-if (sum==num3){
+if (num3==num1+num2){
+System.out.println("True");
+}else if (num2==num3+num1) {
+System.out.println("True");
+}else if (num1==num3+num2) {
 System.out.println("True");
 }else {
 System.out.println("False");
